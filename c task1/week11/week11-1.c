fe18319d26b5b3066cf953f6072c686cb93fde20
mattya-lab <61013032+mattya-lab@users.noreply.github.com>
2020-02-20 13:48:32 +0900

    Add files via upload

---
+++ c task1/week11/week11-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, *b;
+    b = &a
+
+    printf("Input a's value: "); scanf("%d", &a);
+
+    printf("a's value is %d.\n", *b);
+    printf("a's address is %p.\n", &b);
+
+    return 0;
+}
