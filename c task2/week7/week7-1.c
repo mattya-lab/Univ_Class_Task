cf47908f7723600082e1bdb70c5755a70fb8bdc8
mattya-lab <61013032+mattya-lab@users.noreply.github.com>
2020-03-02 21:28:19 +0900

    Add files via upload

---
+++ c task2/week7/week7-1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#define MAX (15)
+void printf_queue_ary(char *q,int top,int rear);
+
+int main(void)
+{
+  char q[MAX]={};
+  int top = 3;
+  int rear = top;
+
+  q[rear]='a'; rear++;
+  q[rear]='b'; rear++;
+  q[rear]='c'; rear++;
+  q[rear]='d'; rear++;
+
+  printf_queue_ary(q,top,rear);
+
+  return 0;
+}
+
+void printf_queue_ary(char *q,int top,int rear)
+{
+  int i;
+
+  printf("%c<---TOP=%d\n",q[top],top);
+  for(i=top+1;i<=rear-1;i++)  
+    printf("%c\n",q[i]);
+  printf("<---REAR=%d\n",rear);
+}
