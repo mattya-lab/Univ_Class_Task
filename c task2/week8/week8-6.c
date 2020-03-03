5650f62915abdc6bd5dd2a09daf6284dde41cabd
mattya-lab <61013032+mattya-lab@users.noreply.github.com>
2020-03-03 15:03:33 +0900

    Add files via upload

---
+++ c task2/week8/week8-6.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct data {
+    char key;
+    struct data* next;
+};
+
+void print_stack_list(struct data* top) {
+    struct data* x;
+    x = top;
+    if (x == NULL) return;
+    else {
+        printf("%c<---TOP\n", x->key);
+        while (x->next != NULL) {
+            x = x->next; printf("%c\n", x->key);
+        }
+    }
+}
+
+void push(struct data** top, char key) {
+    struct data* t;
+    t = (struct data*)malloc(sizeof(struct data));
+    t->key = key; t->next = (*top);
+    (*top) = t;
+}
+
+char pop(struct data** top) {
+    struct data* s;
+    char u;
+    if (*top == NULL) return '\0';
+    else {
+        s = (*top); u = s->key;
+        (*top) = s->next;
+        free(s);
+        return u;
+    }
+}
+
+int main(void) {
+    struct data* top = NULL;
+    char x/*pop_up*/, y/*push_down*/;
+    while (1) {
+        printf("Input a character (0 : Exit, 1 : push_down, other : pop_up)");
+        scanf("%c", &x); getchar();
+        if (x == '0') { printf("終了します\n"); break; }
+        else if (x == '1') {
+            y = pop(&top); printf("The character popped %c.\n", y);
+            print_stack_list(top);
+        }
+        else {
+            push(&top, x);
+            print_stack_list(top);
+        }
+    }
+    return 0;
+}
