8affaa8d61d53641181ba1ef6fe3d6cff27a5a8e
mattya-lab <61013032+mattya-lab@users.noreply.github.com>
2020-03-06 22:01:39 +0900

    Add files via upload

---
+++ c task1/week14/week14-3.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <time.h>
+
+char data_file[] = "datatime.dat";
+
+void get_data(void)
+{
+    FILE* fp;
+    if ((fp = fopen(data_file, "r")) == NULL)
+    {
+        printf("It is first time to execute this program.\n");
+    }
+    else
+    {
+        //Read file and get last operated time  
+        int hour, min, sec;
+        fscanf(fp, "%d %d %d", &hour, &min, &sec);
+        printf("Last operate time was %d hour %d minute %d second.\n", hour, min, sec);
+        fclose(fp);
+    }
+}
+
+void put_data(void)
+{
+    FILE* fp;
+    time_t t = time(&t);
+    struct tm* tm = localtime(&t);
+
+    if ((fp = fopen(data_file, "w")) == NULL)
+        printf("Can't open datatime.dat file.\n");
+    else
+    {
+        //write now time on file
+        fprintf(fp, "%d %d %d\n", tm->tm_hour, tm->tm_min, tm->tm_sec);
+        fclose(fp);
+    }
+}
+
+int main(void)
+{
+    get_data();
+    put_data();
+
+    return 0;
+}
